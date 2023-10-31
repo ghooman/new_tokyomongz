b36143712647da25040f62b37944fcd0a2be6e26
pung8146 <pung8146@naver.com>
2023-10-31 13:49:03 +0900

    상훈 팀스테이킹 포스트 성공

--- src/components/TeamStakingConfirmModal.js
+++ src/components/TeamStakingConfirmModal.js
@@ -9,53 +9,28 @@ const TeamStakingConfirmModal = ({
   teamStakingMomoData,
 }) => {
   const walletAddress = useAddress();
-  console.log("팀크리모달확정 지갑", walletAddress);
-  console.log("팀크리모달확정 몽즈", teamStakingMongzData);
-  console.log("팀크리모달확정 모모", teamStakingMomoData);
-
   const tmhcIds = teamStakingMongzData.id;
   const momoIds = teamStakingMomoData.map((momo) => momo.id);
 
   console.log("몽즈", tmhcIds);
   console.log("모모아이디", momoIds);
   const postTeamStakingNftList = async () => {
-    // const data = {
-    //   address: walletAddress,
-    //   tmhcids: ,
-    //   momoids: ,
-    // };
-    // setDataStatus(false);
-
     try {
       const res = await axios.post(
         "https://mongz-api.sevenlinelabs.app/StakeTeam",
-        // `https://mongz-api.sevenlinelabs.app/StakeTeam?address=${walletAddress}&tmhcIds=${tmhcIds}&momoIds=${momoIds}`
+        {},
         {
           params: {
-            address: JSON.stringify(walletAddress),
+            address: walletAddress,
             tmhcIds: JSON.stringify(tmhcIds),
             momoIds: JSON.stringify(momoIds),
-            // tmhcIds: tmhcIds,
-            // address: walletAddress,
-            // momoIds: momoIds,
           },
         }
       );
       console.log("스테이킹 리스트=========", res);
-      // setStakingData(res.data);
     } catch (err) {
       console.log("스테이킹 리스트 에러 정보 ==========", err);
     } finally {
-      console.log("스테이킹 보내는 정보 =======", walletAddress);
-      console.log("스테이킹 보내는 정보 =======", tmhcIds);
-      console.log("스테이킹 보내는 정보 =======", momoIds);
-      console.log(
-        "스테이킹 보내는 정보json =======",
-        JSON.stringify(walletAddress)
-      );
-      console.log("스테이킹 보내는 정보json =======", JSON.stringify(tmhcIds));
-      console.log("스테이킹 보내는 정보json =======", JSON.stringify(momoIds));
-      // setDataStatus(true);
     }
   };
 

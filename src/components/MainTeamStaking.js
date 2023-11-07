b8ed1a1d2dc93947dba4f3f63d09705c1f1a3b3f
pung8146 <pung8146@naver.com>
2023-11-07 09:02:30 +0900

    상훈 팀스테이킹 페이지 empty 출연 조건 수정

--- src/components/MainTeamStaking.js
+++ src/components/MainTeamStaking.js
@@ -394,14 +394,11 @@ const MainTeamStaking = ({ language, clickStakingMongzData }) => {
   };
   // 클릭한 등급에 따른 모모 리스트
   const filteredMomoNftData = teamStakingMomoData.filter((item) => {
-    console.log("@@@@그레이드상태", gradeState);
-    console.log("아이템랭크", item.rank);
     if (gradeState === "All") {
       return true;
     }
     return item.rank === gradeState;
   });
-  console.log("필터링목록", filteredMomoNftData);
   // 싱글스테이킹, 팀스테이킹 id값 합친 변수
   const finalStakingMomoIds = [...singleStakingMomoIds, ...teamStakingMomoIds];
   return (
@@ -672,7 +669,11 @@ const MainTeamStaking = ({ language, clickStakingMongzData }) => {
                       {(selectedState === "All" ||
                         selectedState === "すべて") && (
                         <span className="header__text--qtt">
-                          {filteredMomoNftData ? filteredMomoNftData.length : 0}
+                          {filteredMomoNftData
+                            ? filteredMomoNftData.filter(
+                                (item) => !finalStakingMomoIds.includes(item.id)
+                              ).length
+                            : 0}
                         </span>
                       )}
                       {(selectedState === "Staking" ||
@@ -756,10 +757,16 @@ const MainTeamStaking = ({ language, clickStakingMongzData }) => {
             <div className="nft__main">
               {isLoading ? (
                 <div className="loading">Now loading...</div>
-              ) : walletAddress === undefined ||
+              ) : // empty가 뜨는조건 지갑이 연결되있지 않거나
+              walletAddress === undefined ||
+                // 보유 모모가 (싱글+팀스테이킹) 전부되어있을경우
                 (teamStakingMomoData?.length ?? 0) ===
                   (singleStakingMomoIds?.length ?? 0) +
-                    (teamStakingMomoIds?.length ?? 0) ? (
+                    (teamStakingMomoIds?.length ?? 0) ||
+                // 필터링된 모모에서 스테이킹중인 모모를 제외하고 NFT가 없다면
+                filteredMomoNftData.filter((item) => {
+                  return finalStakingMomoIds.includes(parseInt(item.id));
+                }).length === 0 ? (
                 <div className="empty-nft">
                   There are no NFTs in possession.
                 </div>
@@ -867,7 +874,6 @@ const MainTeamStaking = ({ language, clickStakingMongzData }) => {
                   selectedState === "未Staking") && (
                   <ul className="main__tmhc-list">
                     {teamStakingMomoData
-
                       .filter((item) => {
                         return !singleStakingMomoIds.includes(
                           parseInt(item.id)
@@ -939,14 +945,14 @@ const MainTeamStaking = ({ language, clickStakingMongzData }) => {
                       selectedState === "Staking" ||
                       selectedState === "Staking中"
                         ? filteredMomoNftData.filter((item) => {
-                            return singleStakingMomoIds.includes(
+                            return finalStakingMomoIds.includes(
                               parseInt(item.id)
                             );
                           }).length
                         : selectedState === "Ready for staking" ||
                           selectedState === "未Staking"
                         ? filteredMomoNftData.filter((item) => {
-                            return !singleStakingMomoIds.includes(
+                            return !finalStakingMomoIds.includes(
                               parseInt(item.id)
                             );
                           }).length

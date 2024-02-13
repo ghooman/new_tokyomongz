c5cda46462fa8c714c2d08cc19eb3d81f10f1fd7
pung8146 <pung8146@naver.com>
2024-02-13 15:32:58 +0900

    상훈 로드맵 이미지 교체

--- src/pages/AboutMomo.js
+++ src/pages/AboutMomo.js
@@ -1,15 +1,11 @@
 import tmhcCoin from "../assets/images/mzc-coin-icon.png";
 // ================ pc img =====================
 import aboutMomo1en from "../assets/images/about-momo-img1-en.png";
-import aboutMomo2en from "../assets/images/about-momo-img2-en.png";
 import aboutMomo1jp from "../assets/images/about-momo-img1-jp.png";
-import aboutMomo2jp from "../assets/images/about-momo-img2-jp.png";
 
 // =============== mo img ======================
 import aboutMomo1enM from "../assets/images/about-momo-img1-en-m.png";
-import aboutMomo2enM from "../assets/images/about-momo-img2-en-m.png";
 import aboutMomo1jpM from "../assets/images/about-momo-img1-jp-m.png";
-import aboutMomo2jpM from "../assets/images/about-momo-img2-jp-m.png";
 
 import momoVid from "../assets/new_hashlink_main_video.mp4";
 
@@ -37,13 +33,6 @@ const AboutMomo = ({ language }) => {
             <img src={aboutMomo1jp} alt="momo1" />
           )}
         </div>
-        <div className="about-momo__img2">
-          {language === "EN" ? (
-            <img src={aboutMomo2en} alt="momo2" />
-          ) : (
-            <img src={aboutMomo2jp} alt="momo2" />
-          )}
-        </div>
 
         {/* ======================= mobile img ====================== */}
         <div className="about-momo__img1-m">
@@ -51,13 +40,6 @@ const AboutMomo = ({ language }) => {
             <img src={aboutMomo1enM} alt="momo1" />
           ) : (
             <img src={aboutMomo1jpM} alt="momo1" />
-          )}
-        </div>
-        <div className="about-momo__img2-m">
-          {language === "EN" ? (
-            <img src={aboutMomo2enM} alt="momo2" />
-          ) : (
-            <img src={aboutMomo2jpM} alt="momo2" />
           )}
         </div>
       </div>
